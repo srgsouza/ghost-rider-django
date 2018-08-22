3633f2994cff8292aebbb19fb76eee3984bbc202
Cameron Binion <ctbdevelop@gmail.com>
2018-08-22 13:34:12 -0600

    added readme text

--- ghost_rider/cars/models.py
+++ ghost_rider/cars/models.py
@@ -5,6 +5,7 @@ from django.dispatch import receiver
 from django.contrib.auth.models import User
 from rest_framework.authtoken.models import Token
 
+
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def create_auth_token(sender, instance=None, created=False, **kwargs):
     if created:
@@ -18,23 +19,27 @@ class Car(models.Model):
     make = models.CharField(max_length=100)
     model = models.CharField(max_length=100)
     year = models.IntegerField()
-    img_url = models.CharField(max_length=100)
-    # images = models.ImageField(upload_to='car_image', blank=True)
-    description = models.CharField(max_length=280)
-    owner = models.ForeignKey('auth.User', related_name='cars', on_delete=models.CASCADE)
+    img_url = models.CharField(max_length=500)
+    description = models.CharField(max_length=1000)
+    owner = models.ForeignKey(
+        'auth.User', related_name='cars', on_delete=models.CASCADE)
 
     def __str__(self):
         return self.make
 
     def save(self, *args, **kwargs):
-        super(Car, self).save(*args, **kwargs)    
-    
+        super(Car, self).save(*args, **kwargs)
+
+
 class Comment(models.Model):
-    comment = models.CharField(max_length=280)
-    car = models.ForeignKey(Car, on_delete=models.CASCADE, related_name='comments')
-    owner = models.ForeignKey('auth.User', related_name='comments', on_delete=models.CASCADE)
+    comment = models.CharField(max_length=1000)
+    car = models.ForeignKey(
+        Car, on_delete=models.CASCADE, related_name='comments')
+    owner = models.ForeignKey(
+        'auth.User', related_name='comments', on_delete=models.CASCADE)
 
     def __str__(self):
         return self.comment
 
-
+# class Images(models.Model):
+#     image = models.ImageField()
